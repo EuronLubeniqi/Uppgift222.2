2c1bc34aa067be68d5121c150ef5b4891c0f91b0
EuronLubeniqi <eulu07001@utb.vaxjo.se>
2024-12-13 08:35:19 +0100

    Add project files.

---
+++ Uppgift222.2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uppgift222._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv ett heltal");
+            String str = Console.ReadLine();
+            int tal=int.Parse(str); 
+            while (tal!=0)
+            {
+                Console.WriteLine("skriv igen (skriv in 0 för att avsluta");
+                str = Console.ReadLine();
+                tal = int.Parse(str);
+            }
+            Console.WriteLine("Du har avslutat programet");
+
+
+
+
+
+
+        }
+
+    }
+}
+    
